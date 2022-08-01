b64b67be1820eddd24f49ed372786473d1e94bd2
root <root@Becky.localdomain>
2022-08-01 15:42:33 -0600

    original

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+from signal import SIGKILL
+from subprocess import CompletedProcess
+from turtle import title
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -5,4 +8,22 @@ db = SQLAlchemy()
 class User(db.Model):
     """a user"""
 
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    fname = db.Column(db.String(20), nullable = False)
+    lname = db.Column(db.String(20), nullable = False)
+    email = db.Column(db.String(50), nullable = False, unique=True)
+    password = db.Column(db.String(20), nullable = False)
+
+
+class Pattern(db.Model):
+    """a pattern"""
+
+    __tablename__ = 'patterns'
+
+    pattern_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50), nullable = False, unique=True)
+    skill = db.Column(db.String(20), nullable = False)
+    type = db.Column(db.String(20), nullable = False)
     
